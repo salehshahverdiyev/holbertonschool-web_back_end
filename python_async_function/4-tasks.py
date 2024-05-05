28454e9936518ef2f3cdc2b3554e4832e189facb
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-05 17:51:43 +0400

    Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Script Documentation
+"""
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        Method Documentation
+    """
+    tasks = [await task_wait_random(max_delay) for _ in range(n)]
+    return sorted(tasks)
