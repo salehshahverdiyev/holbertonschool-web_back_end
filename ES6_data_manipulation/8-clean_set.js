71a28996253c26e2f3fc01faf95f9be858689cd4
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-26 22:02:06 +0400

    Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default function cleanSet(set, startString) {
+    const string = [];
+  
+    if (
+      typeof set !== 'object'
+      || typeof startString !== 'string'
+      || startString.length === 0
+    ) {
+      return '';
+    }
+  
+    for (const item of set) {
+      if (item && item.startsWith(startString)) {
+        string.push(item.slice(startString.length));
+      }
+    }
+    return string.join('-');
+}
