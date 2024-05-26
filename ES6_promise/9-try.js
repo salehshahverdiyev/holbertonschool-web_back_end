5282f0750511ec91d0b9b24ffd546a96a570aaea
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-26 21:29:55 +0400

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+    const queue = [];
+    let value;
+  
+    try {
+      value = mathFunction();
+    } catch (err) {
+      value = err.toString();
+    }
+  
+    queue.push(value);
+    queue.push('Guardrail was processed');
+  
+    return queue;
+}
