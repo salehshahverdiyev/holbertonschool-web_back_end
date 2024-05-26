f449f6463555e8f4194332942de0086ada19a1b7
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2024-05-26 21:18:58 +0400

    Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+    return promise
+      .then(() => ({
+        status: 200,
+        body: 'success',
+      }))
+      .catch(() => Error())
+      .finally(() => console.warn('Got a response from the API'));
+}
